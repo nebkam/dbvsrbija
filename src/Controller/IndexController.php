d1020df8464eb5611fd048a56c2e6d83a9ab9f18
Nebojsa Kamber <nebojsa.kamber@gmail.com>
2021-11-08 23:10:58 +0100

    Favicon Stub pages common.css Navigation (mobile)

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -12,6 +12,18 @@ class IndexController extends AbstractController
 	#[Route('/', methods: [Request::METHOD_GET])]
 	public function index(): Response
 		{
-		return $this->render('base.html.twig');
+		return $this->render('index.html.twig');
+		}
+
+	#[Route('/proizvodi', methods: [Request::METHOD_GET])]
+	public function products(): Response
+		{
+		return $this->render('products.html.twig');
+		}
+
+	#[Route('/o-nama', methods: [Request::METHOD_GET])]
+	public function about(): Response
+		{
+		return $this->render('about.html.twig');
 		}
 	}
